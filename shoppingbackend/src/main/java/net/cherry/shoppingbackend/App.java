238935a40c2c51ca531f941a31f06d7235ea77f0
Ram charan <ramcharankumar5@gmail.com>
2018-01-28 16:13:17 +0530

    my project is successfully created

---
+++ shoppingbackend/src/main/java/net/cherry/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.cherry.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
